d5dedb64c27589376e285ad668a60c64c5f3f1ed
Joechen <s900567@yahoo.com.tw>
2022-05-03 23:55:14 +0800

    First commit

---
+++ Seafood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Seafood
+//
+//  Created by 陳建綸 on 2022/5/3.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
